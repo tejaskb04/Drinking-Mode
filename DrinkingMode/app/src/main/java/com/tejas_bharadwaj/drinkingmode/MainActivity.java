5627d1058d287cd4ceed1386b18c15a945cf5a9e
tejaskb04 <tejaskb@gmail.com>
2017-09-26 09:05:18 -0700

    Cleaning

--- DrinkingMode/app/src/main/java/com/tejas_bharadwaj/drinkingmode/MainActivity.java
+++ DrinkingMode/app/src/main/java/com/tejas_bharadwaj/drinkingmode/MainActivity.java
@@ -1,16 +1,11 @@
 package com.tejas_bharadwaj.drinkingmode;
 
 import android.content.Context;
-import android.net.ConnectivityManager;
 import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,30 +21,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if (clicked) {
+                    drinkingModeBtn.setText("DISABLED");
                     clicked = false;
                 } else {
                     WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
                     wifiManager.setWifiEnabled(false);
-                    setMobileDataEnabled(MainActivity.this, false);
+                    drinkingModeBtn.setText("ENABLED");
                     clicked = true;
                 }
             }
         });
-    }
-
-    private void setMobileDataEnabled(Context context, boolean enabled) {
-        try {
-            final ConnectivityManager conman = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-            final Class conmanClass = Class.forName(conman.getClass().getName());
-            final Field iConnectivityManagerField = conmanClass.getDeclaredField("mService");
-            iConnectivityManagerField.setAccessible(true);
-            final Object iConnectivityManager = iConnectivityManagerField.get(conman);
-            final Class iConnectivityManagerClass = Class.forName(iConnectivityManager.getClass().getName());
-            final Method setMobileDataEnabledMethod = iConnectivityManagerClass.getDeclaredMethod("setMobileDataEnabled", Boolean.TYPE);
-            setMobileDataEnabledMethod.setAccessible(true);
-            setMobileDataEnabledMethod.invoke(iConnectivityManager, enabled);
-        } catch (Exception e) {
-            Toast.makeText(MainActivity.this, "Disabling Internet Failed", Toast.LENGTH_SHORT).show();
-        }
     }
 }
